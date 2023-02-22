57817f8a4109513f7a4faab5260d65cf511c657c
yumi0620 <poilk005@naver.com>
2023-02-22 05:06:22 +0000

    오버워치 퍼블리싱 파일 1차 업로드

---
+++ js/overwatch2.js
@@ -0,0 +1,31 @@
+
+var swiper = new Swiper(".swipeili", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+});
+
+
+var swiper = new Swiper(".swipeooo", {
+  slidesPerView: 1.5,
+  centeredSlides: true,
+  spaceBetween: 30,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
